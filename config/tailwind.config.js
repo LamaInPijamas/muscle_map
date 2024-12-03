69723a953228377616751619320da2addc8bfe43
Ruslan Trytsetskyi <lama.in.pijamas@gmail.com>
2024-12-03 22:46:50 +0100

    Add mainpage demure hero-image and opacity to navbar

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      backgroundImage: {
+        'hero-pattern': "url('images/hero-mountain.jpg')",
+      },
     },
   },
   plugins: [
